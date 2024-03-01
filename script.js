5a3db939e11171e01b03fdd37a11c63322d9a531
Andrei Scafi <andreiscafi@hotmail.com>
2024-03-01 08:36:24 -0300

    Adding calculate function in javascript.

---
+++ script.js
@@ -0,0 +1,8 @@
+//Functions
+const calculate = () => {
+    const value = document.querySelector('#numbers').value;
+    const array = value.split(/,\s*/g);
+    const numbers = array.map(el => Number(el));
+    const filtered = numbers.filter();
+};
+//End of Functions
