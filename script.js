a041d207a343f56a59639a3499a25a90ee40505c
Andrei Scafi <andreiscafi@hotmail.com>
2024-03-02 20:22:23 -0300

    Adding getMode function to javascript part 2.

--- script.js
+++ script.js
@@ -20,6 +20,10 @@ const getMode = (array) => {
     const highest = Object.keys(counts).sort(
         (a, b) => counts[b] - counts[a]
     )[0];
+    const mode = Object.keys(counts).filter(
+        (el) => counts[el] === counts[highest]
+    );
+    return mode.join(", ");
 };
 
 const calculate = () => {
@@ -29,8 +33,10 @@ const calculate = () => {
 
     const mean = getMean(numbers);
     const median = getMedian(numbers);
+    const mode = getMode(numbers);
 
     document.querySelector('#mean').textContent = mean;
     document.querySelector('#median').textContent = median;
+    document.querySelector('#mode').textContent = mode;
 };
 //End of Functions
